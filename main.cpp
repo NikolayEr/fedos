cf9f98f71a1c9085f0a9370d2bd597d8dffc3fb1
NikolayEr <hakccii@gmail.com>
2024-12-25 23:51:43 +0300

    Add file main

--- main.cpp
+++ main.cpp
@@ -1,5 +1,12 @@
 #include "sdt.h"
+#include <iostream>
+using namespace std;
 
 int main() {
+    int a, b;
+    cout << "Enter the first number: ";
+    cin >> a;
+    cout << "Enter the second number: ";
+    cin >> b;
     return 0;
 }
